a7c9799d6f8724f6ffe90efae3acc15a8f12ac69
Swirl86 <swesuzzi@gmail.com>
2019-03-07 23:40:51 +0100

    Added text file import read for sensor data, work started on time.sleep ongoing

--- main.py
+++ main.py
@@ -1,67 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 # https://stackoverflow.com/questions/10752055/cross-origin-requests-are-only-supported-for-http-error-when-loading-a-local
 # http://flask.pocoo.org/docs/1.0/quickstart/#static-files
 # Run command: python main.py
 
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 
 from flask import Flask, json, request
 from flask import render_template
 from flask import redirect, url_for
-import datetime
+import time
+from threading import Thread
 
 app = Flask(__name__)  # Construct an instance of Flask class for our webapp
 
 
 @app.route("/")  # URL '/' to be handled by main() route handler
 def home():
-    now = datetime.datetime.now()
-    dateString = now.strftime("%Y-%m-%d")
-    timeString = now.strftime("%H:%M")
+    # Waiter().start()  # Vänta 30min
 
-    dummyData = [
-        {
-            'Date': dateString,
-            'Time': timeString,
-            'Humidity': 530,
-            'Watered': '09:50'
-        },
-        {
-            'Date': dateString,
-            'Time': timeString,
-            'Humidity': 630,
-            'Watered': '00:50'
-        },
-        {
-            'Date': dateString,
-            'Time': timeString,
-            'Humidity': 430,
-            'Watered': '09:50'
-        }
-    ]
-    return render_template('index.html', sensor=dummyData)
+    filedata = []
+    with open('static/test.txt') as fp:  # NOTERA: uppdatera filvägen
+        for line in fp:
+            str = line
+            dataarray = str.split(',')
+            it = iter(dataarray)
+            ID1 = it.next()
+            ID2 = it.next()
+            ID3 = it.next()
+            ID4 = it.next()
+            theData = {
+                'Date': ID2,
+                'Time': ID3,
+                'Moisture': ID1,
+                'Watered': ID4
+            }
+            filedata.append(theData)
 
-# Test för senare http://mattrichardson.com/Raspberry-Pi-Flask/
-# https://www.fontenay-ronan.fr/web-interface-for-raspberrys-sensors-gpio/
-# https://electronicshobbyists.com/how-to-set-up-a-raspberry-pi-web-server/
-# https://learn.adafruit.com/raspipe-a-raspberry-pi-pipeline-viewer-part-2/miniature-web-applications-in-python-with-flask
-@app.route("/readPin/<pin>")
-def readPin(pin):
-    try:
-        GPIO.setup(int(pin), GPIO.IN)
-        if GPIO.input(int(pin)) == True:
-            response = "Pin number " + pin + " is high!"
-        else:
-            response = "Pin number " + pin + " is low!"
-    except:
-        response = "There was an error reading pin " + pin + "."
+    return render_template('index.html', sensor=filedata)
 
-    templateData = {
-        'title': 'Status of Pin' + pin,
-        'response': response
-    }
 
-    return render_template('pin.html', **templateData)
+# class Waiter(Thread):
+#     def run(self):
+#         x = 0
+#         while x <= 180:
+#             time.sleep(10)
+#             x += 1
 
 
 if __name__ == "__main__":  # Script executed directly?
